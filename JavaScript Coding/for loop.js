d6d2c2140c9aa49783f6efa6894673c4380bf00a
pushp1502 <pushpanjali201818@gmail.com>
2024-05-31 00:24:35 +0530

    Practicing for loop with CodeByHeart

---
+++ JavaScript Coding/for loop.js
@@ -0,0 +1,16 @@
+for( let sn = 1; sn<=10; sn++)
+{ //blocked for loop
+    console.log(sn +  " india");
+
+}
+console.log("----------------------")
+
+//with not block loop
+let sn1
+for( sn1 = 1; sn1<=10; sn1++)
+    { //blocked for loop
+        console.log(sn1 +  " india");
+    
+}
+console.log(sn1)
+    
